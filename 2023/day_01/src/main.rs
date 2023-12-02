ea83478910c71a37f55a098c9a73b7db70bea33c
rrohrer <ryan.rohrer@gmail.com>
2023-12-01 16:14:47 -0800

    works

--- 2023/day_01/src/main.rs
+++ 2023/day_01/src/main.rs
@@ -1,44 +1,62 @@
-use aho_corasick::{AhoCorasick, MatchKind};
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
-fn get_calibration(line: String) -> i32 {
-    let first = *line
-        .chars()
-        .filter(|c| c.is_digit(10))
-        .collect::<Vec<char>>()
-        .first()
-        .unwrap();
-    let second = *line
-        .chars()
-        .filter(|c| c.is_digit(10))
-        .collect::<Vec<char>>()
-        .last()
-        .unwrap();
-    let s = format!("{}{}", first, second);
-    s.parse::<i32>().unwrap()
+fn get_digit(s: &str) -> char {
+    match s {
+        "1" => '1',
+        "one" => '1',
+        "2" => '2',
+        "two" => '2',
+        "3" => '3',
+        "three" => '3',
+        "4" => '4',
+        "four" => '4',
+        "5" => '5',
+        "five" => '5',
+        "6" => '6',
+        "six" => '6',
+        "7" => '7',
+        "seven" => '7',
+        "8" => '8',
+        "eight" => '8',
+        "9" => '9',
+        "nine" => '9',
+        _ => panic!("Invalid!"),
+    }
 }
 
-fn cleanup_string(line: String, ac: &AhoCorasick) -> String {
-    let r = &["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    ac.replace_all(&line, r)
+fn calibration(line: String) -> i32 {
+    let p = [
+        "1", "one", "2", "two", "3", "three", "4", "four", "5", "five", "6", "six", "7", "seven",
+        "8", "eight", "9", "nine",
+    ];
+    let mut matches = Vec::new();
+    for pattern in p.iter() {
+        matches.extend(line.match_indices(pattern));
+    }
+
+    matches.sort_by(|a, b| {
+        if a.0 < b.0 {
+            Ordering::Less
+        } else {
+            Ordering::Greater
+        }
+    });
+
+    let first = matches.first().unwrap();
+    let last = matches.last().unwrap();
+    let s = format!("{}{}", get_digit(first.1), get_digit(last.1));
+    s.parse::<i32>().unwrap()
 }
 
 fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
 
-    let p = &[
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
-    let ac = AhoCorasick::builder()
-        .match_kind(MatchKind::LeftmostFirst)
-        .build(p)
-        .unwrap();
-
     let mut sum = 0;
     for line in reader.lines() {
-        sum += get_calibration(cleanup_string(line.unwrap(), &ac));
+        sum += calibration(line.unwrap());
     }
 
     println!("sum: {}", sum);
